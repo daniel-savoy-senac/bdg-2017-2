0ca436a49f6c459c1b866ff54760584089910329
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-08-09 11:01:55 -0300

    height

--- Turma A/Aula 01/programa.js
+++ Turma A/Aula 01/programa.js
@@ -1,7 +1,7 @@
 //alert("EBAAA!!!");
 
 function mudar(evt){
-  let tam = window.innerWidth;
+  let tam = window.innerWidth; //window.innerHeight
   let cor = (evt.x / tam) * 360;
   console.log(cor);
   document.body.style.background = `hsl(${cor},100%,50%)`;
