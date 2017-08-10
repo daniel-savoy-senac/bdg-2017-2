802c170d6bf4dc7a9c7336dcbf395e3a170b4729
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-08-10 10:49:27 -0300

    cor

--- Turma B/Aula 01/programa.js
+++ Turma B/Aula 01/programa.js
@@ -4,7 +4,7 @@ function mudar(evt){
   let x = evt.x;
   let y = evt.y;
   console.log(`x: ${x}, y: ${y}`);
-  document.body.style.background = "red";
+  document.body.style.background = `hsl(${x},100%,50%)`;
 }
 
 window.addEventListener("click", mudar);
