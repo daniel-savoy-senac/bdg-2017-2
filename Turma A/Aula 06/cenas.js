8a8a4e0fe589064da6417d454ed19328215e6f43
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-09-13 09:13:31 -0300

    cenas

--- Turma A/Aula 06/cenas.js
+++ Turma A/Aula 06/cenas.js
@@ -1,9 +1,10 @@
-const cenas = ["cena1","cena2","cena3"];
+const cenas = ["cena1","cena2","cena3","cena4"];
 let atual = 0;
 
 function mudar(){
   document.body.className = cenas[atual];
   atual = atual + 1;
+  if(atual >= cenas.length) atual = 0;
 }
 
 window.addEventListener("click", mudar);
