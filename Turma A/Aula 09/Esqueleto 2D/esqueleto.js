fcd1972bc0297188dc98dc89da6898b6e432ded1
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-10-04 11:03:26 -0300

    js

--- Turma A/Aula 09/Esqueleto 2D/esqueleto.js
+++ Turma A/Aula 09/Esqueleto 2D/esqueleto.js
@@ -2,6 +2,11 @@ function animar(evt){
 	let tempo = evt.x / window.innerWidth;
 	let a = lerp(tempo, -60, 60);
 	girar(".cotovelo.dir", a);
+
+	girar(".cotovelo.esq", lerp(tempo, -120, 0));
+	girar(".perna.esq", lerp(tempo, 300, 0));
+	girar(".joelho.esq", lerp(tempo, 120, 0));
+	girar(".pe.esq", lerp(tempo, -40, 0));
 }
 
 function girar(query, angulo){
