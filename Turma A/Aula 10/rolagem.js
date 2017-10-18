c5433d88057b3cab9005786019320dd1a43c1970
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-10-18 10:22:04 -0200

    tempo

--- Turma A/Aula 10/rolagem.js
+++ Turma A/Aula 10/rolagem.js
@@ -1,3 +1,12 @@
+let t = 0;
+
 function lerp(tempo, A, B){
 	return B * tempo + A * (1 - tempo);
 }
+
+function cronometrar(evt){
+	t = evt.x / window.innerWidth;
+}
+
+
+window.addEventListener("mousemove", cronometrar);
