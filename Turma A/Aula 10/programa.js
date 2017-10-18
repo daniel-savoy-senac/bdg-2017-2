fbbdff8ad967c22adb274cb649c135d2aa78e327
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-10-18 10:50:12 -0200

    js

--- Turma A/Aula 10/programa.js
+++ Turma A/Aula 10/programa.js
@@ -1,7 +1,18 @@
 let caixa;
+let cor = 0;
 
 function inicio(){
-  caixa = document.querySelector("#caixa");
+  caixa = document.querySelector(".caixa");
+  animar();
+}
+
+function animar(){
+  let angulo = lerp(r , 0, 720);
+  caixa.style.transform = `rotate(${angulo}deg)`;
+  cor = cor + 1;
+  caixa.style.background = `hsl(${cor}, 100%, 50%)`;
+
+  window.requestAnimationFrame(animar);
 }
 
 window.addEventListener("load", inicio);
