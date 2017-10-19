869045ad9960800b4b892ad309d9964ab7b4050a
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-10-19 11:34:02 -0200

    ajustes

--- Turma B/Aula 08/lerp.js
+++ Turma B/Aula 08/lerp.js
@@ -7,9 +7,14 @@ function lerp(tempo, A, B) {
 function tempoMouse(evt) {
   tm = evt.x / window.innerWidth;
   let a = lerp(tm, 45, 720);
+  let cor = ~~lerp(tm, 255, 0);
+  let l = lerp(tm, 0, 360);
+
 
   let caixa = document.querySelector(".caixa");
   caixa.style.transform = `rotate(${a}deg)`;
+  //caixa.style.background = `rgb(${cor},0,0)`;
+  caixa.style.background = `hsl(${l},100%,50%)`;
 }
 
 window.addEventListener("mousemove", tempoMouse);
