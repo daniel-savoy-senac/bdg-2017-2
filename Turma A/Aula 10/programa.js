d396af3015d5b349cf8e15cde775952194e8ce53
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-10-19 09:25:57 -0200

    temas turma b

--- Turma A/Aula 10/programa.js
+++ Turma A/Aula 10/programa.js
@@ -7,8 +7,9 @@ function inicio(){
 }
 
 function animar(){
-  let angulo = lerp(r , 0, 720);
-  caixa.style.transform = `rotate(${angulo}deg)`;
+  let angulo = lerp(t , 0, 720);
+  let desloc = lerp(r , 0, 720);
+  caixa.style.transform = `rotate(${angulo}deg) translateX(${desloc}px)`;
   cor = cor + 1;
   caixa.style.background = `hsl(${cor}, 100%, 50%)`;
 
