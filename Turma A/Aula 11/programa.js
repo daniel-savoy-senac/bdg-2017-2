92aab1ba9ce41c571ce3b23328cd1c2c4d286976
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-10-25 09:31:01 -0200

    cenas

--- Turma A/Aula 11/programa.js
+++ Turma A/Aula 11/programa.js
@@ -6,14 +6,17 @@ function inicio(){
 }
 
 function animar(){
-  let t1 = clampCena(r, 0, 0.5);
-  let t2 = clampCena(r, 0.5, 1);
+  let t1 = clampCena(r, 0, 1/3);
+  let t2 = clampCena(r, 1/3, 2/3);
+  let t3 = clampCena(r, 2/3, 1);
 
   let x = lerp(t1, 0, 800);
   let y = lerp(t2, 0, 400);
+  let x2 = lerp(t3, 800, 0);
 
   caixa.style.left = `${x}px`;
   caixa.style.top = `${y}px`;
+  if(t3>0) caixa.style.left = `${x2}px`;
 
   window.requestAnimationFrame(animar);
 }
