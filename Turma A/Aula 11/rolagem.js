af3d821d458e342a87ac0e8dd9dc016c02ec86cb
DANIEL PRATA SAVOY <daniel.psavoy@CAS411234MAC25.local>
2017-10-25 08:50:42 -0200

    cenas

--- Turma A/Aula 11/rolagem.js
+++ Turma A/Aula 11/rolagem.js
@@ -1,12 +1,27 @@
-let t = 0;
+let m = 0;
 let r = 0;
 
 function lerp(tempo, A, B){
 	return B * tempo + A * (1 - tempo);
 }
 
+function clampCena(tempo, inicio, fim){
+	return clamp(cena(tempo, inicio, fim));
+}
+
+function cena(tempo, inicio, fim){
+	let delta = fim - inicio;
+	return (tempo - inicio) / delta;
+}
+
+function clamp(numero){
+	if(numero < 0) return 0;
+	if(numero > 1) return 1;
+	return numero;
+}
+
 function mouse(evt){
-	t = evt.x / window.innerWidth;
+	m = evt.x / window.innerWidth;
 }
 
 function barra(evt){
